9cd53af74a65557dfde1d12f96e16637d4a587d8
baeniel <jinveloper126@gmail.com>
2020-04-27 10:58:42 +0900

    회원가입 오류 fix

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 2020_04_15_075229) do
     t.string "phone"
     t.string "username"
     t.boolean "fit_center"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
