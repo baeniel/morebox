b79e290f20d480fce4286166042c3d4caa9c20d0
baeniel <jinveloper126@gmail.com>
2020-05-19 14:50:32 +0900

    complete

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -7,6 +7,7 @@ class ApisController < ApplicationController
       },
       body: {
         cid: "CT24824054", # 발급받은 cid 값
+        # cid: "TC0ONETIME", # 발급받은 cid 값
         partner_order_id: '12345678', # 가맹점 주문 번호
         partner_user_id: '123', # 가맹점 회원 id
         item_name: '고릴라밤',
@@ -14,16 +15,18 @@ class ApisController < ApplicationController
         total_amount: params[:price],
         vat_amount: 0,
         tax_free_amount: 0,
-        approval_url: "http://localhost:3000/items/#{params[:id]}/",
-        # approval_url: "http://morebox.co.kr/orders/#{params[:id]}/complete",
-        fail_url: 'http://localhost:3000/',
-        # fail_url: 'http://morebox.co.kr/',
-        cancel_url: 'http://localhost:3000/',
-        # cancel_url: 'http://morebox.co.kr/'
+        # approval_url: "http://localhost:3000/items/#{params[:id]}/",
+        approval_url: "http://morebox.co.kr/items/#{params[:id]}/",
+        # fail_url: 'http://localhost:3000/',
+        fail_url: 'http://morebox.co.kr/',
+        # cancel_url: 'http://localhost:3000/',
+        cancel_url: 'http://morebox.co.kr/'
       }
     )
 
     data = response.parsed_response
+    cookies[:tid] = data.first.second
     render json: data
+
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,19 @@ class ItemsController < ApplicationController
 
     if params[:pg_token].present?
       current_user.update_attributes(payment: true)
+      response = HTTParty.post(
+        "https://kapi.kakao.com/v1/payment/approve",
+        headers: {
+          Authorization: "KakaoAK f348a6522071ea17f9dabce9a88b0744"
+        },
+        body: {
+          cid: "CT24824054", # 발급받은 cid 값
+          tid: cookies[:tid],
+          partner_order_id: '12345678', # 가맹점 주문 번호
+          partner_user_id: '123', # 가맹점 회원 id
+          pg_token: params[:pg_token]
+        }
+      )
     end
   end
 
