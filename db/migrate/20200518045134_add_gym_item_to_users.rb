bc7a6f8c0a2dfa8e3e3987d6b4da2f1f568771ba
baeniel <jinveloper126@gmail.com>
2020-05-22 10:20:03 +0900

    fix

--- db/migrate/20200518045134_add_gym_item_to_users.rb
+++ db/migrate/20200518045134_add_gym_item_to_users.rb
@@ -1,6 +1,6 @@
 class AddGymItemToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :gym, null: false, foreign_key: true
+    add_reference :users, :gym, null: true, foreign_key: true
     add_reference :users, :item, null: false, foreign_key: true
   end
 end
