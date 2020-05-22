7a4d98c466504f86a14d2c22cd7e4eddb133d3f1
baeniel <jinveloper126@gmail.com>
2020-05-22 11:53:38 +0900

    change

--- db/migrate/20200522024305_changedefaultvalueforgym.rb
+++ db/migrate/20200522024305_changedefaultvalueforgym.rb
@@ -1,7 +1,7 @@
 class Changedefaultvalueforgym < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :gyms, :ultra_purchase, default: 0
-    change_column_default :gyms, :gorilla_purchase, default: 0
-    change_column_default :gyms, :protein_purchase, default: 0
+    change_column_default :gyms, :ultra_purchase, :integer, default: 0
+    change_column_default :gyms, :gorilla_purchase, :integer, default: 0
+    change_column_default :gyms, :protein_purchase, :integer, default: 0
   end
 end
