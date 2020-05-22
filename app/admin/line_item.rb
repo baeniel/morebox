f0146c27b30e8beb0a8e0bd6b1eaaeac075285ae
baeniel <jinveloper126@gmail.com>
2020-05-22 11:09:17 +0900

    admin

--- app/admin/line_item.rb
+++ app/admin/line_item.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register LineItem do
+  permit_params :title, :quantity, :order_id, :temp
 
+  index do
+    selectable_column
+    id_column
+    column :title
+    # column :image do |line_item|
+    #   image_tag(line_item.image_url, class: 'admin-index-image')
+    # end
+    column :order_id
+    column "username" do |line_item|
+      line_item.order.user.phone
+    end
+    column "item" do |line_item|
+      line_item.order.item
+    end
+    column :quantity
+    column :temp
+    column :created_at
+    actions
+  end
 end
