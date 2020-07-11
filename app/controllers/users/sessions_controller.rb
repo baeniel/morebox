cdb58e68591e04745e6da4f3f2ee93de740e6c39
baeniel <jinveloper126@gmail.com>
2020-07-11 14:40:02 +0900

    회원가입, 로그인 절차 및 ui 개선

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,13 +5,16 @@ class Users::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
   def new
-    if params[:user].present? && User.pluck(:phone).exclude?(params[:user][:phone])
-      flash[:notice] = "존재하지 않는 전화번호입니다."
+    if params[:user].present? && User.pluck(:phone).exclude?(params.dig(:user,:phone))
+      # flash[:notice] = "존재하지 않는 전화번호입니다."
+      flash[:notice] = "회원가입이 필요합니다."
+      redirect_to new_user_registration_path(phone: params.dig(:user,:phone))
+    else
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      yield resource if block_given?
+      respond_with(resource, serialize_options(resource))
     end
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
   end
 
   # POST /resource/sign_in
