fe0069ac21850acb1a2022c6b44e7d3754b12b44
baeniel <jinveloper126@gmail.com>
2020-07-15 15:24:09 +0900

    fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,8 +18,8 @@ class OrdersController < ApplicationController
               Authorization: "KakaoAK f348a6522071ea17f9dabce9a88b0744"
             },
             body: {
-              # cid: "CT24824054",
-              cid: "TC0ONETIME",
+              cid: "CT24824054",
+              # cid: "TC0ONETIME",
               tid: cookies[:tid],
               partner_order_id: "#{gym.id}", # 가맹점 주문 번호
               partner_user_id: "#{current_user.id}", # 가맹점 회원 id
@@ -67,133 +67,56 @@ class OrdersController < ApplicationController
   end
 
   def update
-    @item = @order.item
-
-    @order.line_items.where.not(temp: 0).each do |line_item|
-      line_item.increment!('quantity', line_item.temp)
-      line_item.increment!('point', line_item.temp * SubItem.find_by(title: line_item.title).point)
-    end
-
-    @order.update_attributes(number: @order.line_items.sum(:quantity))
-
-    if current_user.orders.sum(:point) - current_user.line_items.sum(:point) <= 2000
-      corpNum = "7468701862"
-      userID = "jb1014"
-      sender = "01056053087"
-      senderName = "주식회사 고릴라밤"
-      receiver = @order.user.phone
-      receiverName = @order.user.phone.last(4)
-      contents = "[MoreBox]\n"+"아래 링크로 접속하셔서\n헬스장 오기 전에 미리 결제하세요:)\n\nhttps://morebox.co.kr"
-      adsYN = false
-      reserveDT = ""
-      requestNum = ''
-
-      begin
-        @value = OrdersController::MSGService.sendSMS(
-          corpNum,
-          sender,
-          senderName,
-          receiver,
-          receiverName,
-          contents,
-          reserveDT,
-          adsYN,
-          userID,
-          requestNum,
-        )
-        @name = "receiptNum(접수번호)"
-
-      rescue PopbillException => pe
-        @Response = pe
-        redirect_to home_exception_path
-      end
-    else
-      templateCode = '020060000176'
-      content = "[MoveMore]\n"+@order.user.phone.last(4)+"님의 "+@order.line_items.where("temp is NOT NULL and temp != 0").map { |item| item.title }.flatten.join(' ')+" "+@order.line_items.sum(:temp).to_s+"개 꺼내기가 완료되었습니다.\n\n현재 잔여 포인트: "+(current_user.orders.sum(:point) - current_user.line_items.sum(:point)).to_s+"포인트\n\n문의사항 있으시면 무브모어 카카오톡 채널로 편하게 연락주시기 바랍니다.\n\n무브모어 카카오톡 채널: @무브모어 @movemore"
-      altContent = '대체문자 내용 입니다'
-      # 대체문자 유형 (공백-미전송 / C-알림톡내용 / A-대체문자내용)
-      altSendType = 'C'
-      sndDT = ''
-      receiverName = @order.user.phone.last(4)
-      receiver = @order.user.phone
-      corpNum = "7468701862"
-      userID = "jb1014"
-      snd = '010-5605-3087'
-      requestNum = ''
-
-      begin
-        @value = OrdersController::KakaoService.sendATS_one(
-            corpNum,
-            templateCode,
-            snd,
-            content,
-            altContent,
-            altSendType,
-            sndDT,
-            receiver,
-            receiverName,
-            requestNum,
-            userID,
-        )['receiptNum']
-        @name = "receiptNum(접수번호)"
-
-      rescue PopbillException => pe
-        @Response = pe
-        redirect_to home_exception_path
-      end
-    end
-
     #데이터베이스 재고 갱신
-    @gym = current_user.gym
-    gym_stock
-    @gym.update_attributes(ultra_stock: @ultra_stock, gorilla_stock: @gorilla_stock, protein_stock: @protein_stock, stock_1: @stock_1, stock_2: @stock_2, stock_3: @stock_3, stock_4: @stock_4)
-
-    @gyms = Gym.where(gorilla_stock: 15..20).or(Gym.where(ultra_stock: 15..20)).or(Gym.where(protein_stock: 15..20)).or(Gym.where(stock_1: 15..20))
-    if @gyms.present?
-      templateCode = '020050000216'
-      receiverName = '박진배'
-      receiver = '010-5605-3087'
-      corpNum = "7468701862"
-      userID = "jb1014"
-      snd = '010-5605-3087'
-      altContent = '대체문자 내용 입니다'
-      # 대체문자 유형 (공백-미전송 / C-알림톡내용 / A-대체문자내용)
-      altSendType = 'C'
-      sndDT = ''
-      requestNum = ''
-
-      hash = { gorilla: @gym.gorilla_stock, ultra: @gym.ultra_stock, protein: @gym.protein_stock, stock_1: @gym.stock_1, stock_2: @gym.stock_2, stock_3: @gym.stock_3 }
-      arr = []
-      hash.map.each do |k, v|
-        if v > 0 && v < 15
-          arr << k
-        end
-      end
-
-      content = @gym.title+" 센터의 "+arr.map(&:inspect).join(',')+" 재고가 곧 소진됩니다. 센터에 배송해주십시오."
-
-      begin
-        @value = OrdersController::KakaoService.sendATS_one(
-            corpNum,
-            templateCode,
-            snd,
-            content,
-            altContent,
-            altSendType,
-            sndDT,
-            receiver,
-            receiverName,
-            requestNum,
-            userID,
-        )['receiptNum']
-        @name = "receiptNum(접수번호)"
-
-      rescue PopbillException => pe
-        @Response = pe
-        redirect_to home_exception_path
-      end
-    end
-
+    # @gym = current_user.gym
+    # gym_stock
+    # @gym.update_attributes(ultra_stock: @ultra_stock, gorilla_stock: @gorilla_stock, protein_stock: @protein_stock, stock_1: @stock_1, stock_2: @stock_2, stock_3: @stock_3, stock_4: @stock_4)
+    #
+    # @gyms = Gym.where(gorilla_stock: 15..20).or(Gym.where(ultra_stock: 15..20)).or(Gym.where(protein_stock: 15..20)).or(Gym.where(stock_1: 15..20))
+    # if @gyms.present?
+    #   templateCode = '020050000216'
+    #   receiverName = '박진배'
+    #   receiver = '010-5605-3087'
+    #   corpNum = "7468701862"
+    #   userID = "jb1014"
+    #   snd = '010-5605-3087'
+    #   altContent = '대체문자 내용 입니다'
+    #   # 대체문자 유형 (공백-미전송 / C-알림톡내용 / A-대체문자내용)
+    #   altSendType = 'C'
+    #   sndDT = ''
+    #   requestNum = ''
+    #
+    #   hash = { gorilla: @gym.gorilla_stock, ultra: @gym.ultra_stock, protein: @gym.protein_stock, stock_1: @gym.stock_1, stock_2: @gym.stock_2, stock_3: @gym.stock_3 }
+    #   arr = []
+    #   hash.map.each do |k, v|
+    #     if v > 0 && v < 15
+    #       arr << k
+    #     end
+    #   end
+    #
+    #   content = @gym.title+" 센터의 "+arr.map(&:inspect).join(',')+" 재고가 곧 소진됩니다. 센터에 배송해주십시오."
+    #
+    #   begin
+    #     @value = OrdersController::KakaoService.sendATS_one(
+    #         corpNum,
+    #         templateCode,
+    #         snd,
+    #         content,
+    #         altContent,
+    #         altSendType,
+    #         sndDT,
+    #         receiver,
+    #         receiverName,
+    #         requestNum,
+    #         userID,
+    #     )['receiptNum']
+    #     @name = "receiptNum(접수번호)"
+    #
+    #   rescue PopbillException => pe
+    #     @Response = pe
+    #     redirect_to home_exception_path
+    #   end
+    # end
   end
 
   def show
