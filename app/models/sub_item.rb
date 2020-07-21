9ee2f596ad3d54a181bdc30c8733fdc3ef7b3f8a
baeniel <jinveloper126@gmail.com>
2020-07-21 16:17:15 +0900

    Fix

--- app/models/sub_item.rb
+++ app/models/sub_item.rb
@@ -2,6 +2,6 @@ class SubItem < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_and_belongs_to_many :gyms, join_table: :gyms_sub_items
-  has_many :points
+  has_many :points, dependent: :nullify
   # accepts_nested_attributes_for :gyms
 end
