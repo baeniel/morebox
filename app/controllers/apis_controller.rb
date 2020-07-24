f9535bd929947e0c125727e77d436d124d87f087
baeniel <jinveloper126@gmail.com>
2020-07-24 14:27:28 +0900

    back kakao

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -2,86 +2,86 @@ class ApisController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
-  # def pay_url
-  #   # 카카오페이 버전
-  #   item = Item.find_by(id: params[:item_id])
-  #   gym = Gym.find_by(id: params[:gym_id])
-  #   if item && gym
-  #     response = HTTParty.post(
-  #       "https://kapi.kakao.com/v1/payment/ready",
-  #       headers: {
-  #         Authorization: "KakaoAK f348a6522071ea17f9dabce9a88b0744"
-  #       },
-  #       body: {
-  #         cid: "CT24824054",
-  #         # cid: "TC0ONETIME",
-  #         partner_order_id: "#{gym.id}", # 가맹점 주문 번호
-  #         partner_user_id: "#{current_user.id}", # 가맹점 회원 id
-  #         item_name: "#{item.title}",
-  #         quantity: 1,
-  #         total_amount: item.price,
-  #         vat_amount: 0,
-  #         tax_free_amount: 0,
-  #         # approval_url: "http://localhost:3000/orders/payment?item_id=#{item.id}",
-  #         approval_url: "https://morebox.co.kr/orders/payment?item_id=#{item.id}",
-  #         # fail_url: 'http://localhost:3000/',
-  #         fail_url: 'https://morebox.co.kr/',
-  #         # cancel_url: 'http://localhost:3000/',
-  #         cancel_url: 'https://morebox.co.kr/'
-  #       }
-  #     )
-  #
-  #     data = response.parsed_response
-  #     cookies[:tid] = data.first.second
-  #     render json: data
-  #   else
-  #     render json: {result: false}
-  #   end
-  # end
-
-  # 부트페이 버전
   def pay_url
+    # 카카오페이 버전
     item = Item.find_by(id: params[:item_id])
-    session[:passed_id] = item.id
-    session[:passed_price] = item.price
-
-    # partner_order_id: "#{gym.id}",
-    # partner_user_id: "#{current_user.id}",
-
-    bootpay = Bootpay::ServerApi.new(
-      "5eb2230002f57e002d1edd8d",
-      "GAx0ZCkgGIZuKMlfLgWDbOpAlpSVYV5IWXdmBKURELg="
-    )
-
-    result  = bootpay.get_access_token
-
-    if result[:status] == 200
-      response = bootpay.request_payment(
-        pg: 'inicis', # PG Alias
-        method: 'card', # Method Alias
-        order_id: (Time.current.to_i), # 사용할 OrderId
-        price: item.price, # 결제금액
-        name: "#{item.title}", # 상품명
-        # return_url: 'http://localhost:3000/apis/pay_complete'
-        return_url: 'https://morebox.co.kr/apis/pay_complete'
+    gym = Gym.find_by(id: params[:gym_id])
+    if item && gym
+      response = HTTParty.post(
+        "https://kapi.kakao.com/v1/payment/ready",
+        headers: {
+          Authorization: "KakaoAK f348a6522071ea17f9dabce9a88b0744"
+        },
+        body: {
+          cid: "CT24824054",
+          # cid: "TC0ONETIME",
+          partner_order_id: "#{gym.id}", # 가맹점 주문 번호
+          partner_user_id: "#{current_user.id}", # 가맹점 회원 id
+          item_name: "#{item.title}",
+          quantity: 1,
+          total_amount: item.price,
+          vat_amount: 0,
+          tax_free_amount: 0,
+          # approval_url: "http://localhost:3000/orders/payment?item_id=#{item.id}",
+          approval_url: "https://morebox.co.kr/orders/payment?item_id=#{item.id}",
+          # fail_url: 'http://localhost:3000/',
+          fail_url: 'https://morebox.co.kr/',
+          # cancel_url: 'http://localhost:3000/',
+          cancel_url: 'https://morebox.co.kr/'
+        }
       )
 
-      link = response[:data]
-      receiver = current_user.phone
-      receiverName = current_user.phone.last(4)
-      contents = "[MoreBox]\n"+"아래 링크로 접속하셔서 결제하세요!\n"+"#{link}"
-      payment_alarm = MessageAlarmService.new(receiver, receiverName, contents)
-      payment_alarm.send_message
-
-      # link = response[:data]
-      # templateCode = '020050000281'
-      # content = "[MoveMore]\n"+link+" 님의 사용권이 소진되었습니다ㅠ\n\n아래 버튼으로 결제하시고 헬스장에\n있는 태블릿으로 체크인 하시면 됩니다:)\n\n\n무브모어 카카오톡 채널:\n@무브모어 @movemore"
-      # receiver = current_user.phone
-      # receiverName = current_user.phone.last(4)
-      # kakao_boot = KakaoAlarmService.new(templateCode, content, receiver, receiverName)
-      # kakao_boot.send_alarm
+      data = response.parsed_response
+      cookies[:tid] = data.first.second
+      render json: data
+    else
+      render json: {result: false}
     end
   end
+
+  # 부트페이 버전
+  # def pay_url
+  #   item = Item.find_by(id: params[:item_id])
+  #   session[:passed_id] = item.id
+  #   session[:passed_price] = item.price
+  #
+  #   # partner_order_id: "#{gym.id}",
+  #   # partner_user_id: "#{current_user.id}",
+  #
+  #   bootpay = Bootpay::ServerApi.new(
+  #     "5eb2230002f57e002d1edd8d",
+  #     "GAx0ZCkgGIZuKMlfLgWDbOpAlpSVYV5IWXdmBKURELg="
+  #   )
+  #
+  #   result  = bootpay.get_access_token
+  #
+  #   if result[:status] == 200
+  #     response = bootpay.request_payment(
+  #       pg: 'inicis', # PG Alias
+  #       method: 'card', # Method Alias
+  #       order_id: (Time.current.to_i), # 사용할 OrderId
+  #       price: item.price, # 결제금액
+  #       name: "#{item.title}", # 상품명
+  #       # return_url: 'http://localhost:3000/apis/pay_complete'
+  #       return_url: 'https://morebox.co.kr/apis/pay_complete'
+  #     )
+  #
+  #     link = response[:data]
+  #     receiver = current_user.phone
+  #     receiverName = current_user.phone.last(4)
+  #     contents = "[MoreBox]\n"+"아래 링크로 접속하셔서 결제하세요!\n"+"#{link}"
+  #     payment_alarm = MessageAlarmService.new(receiver, receiverName, contents)
+  #     payment_alarm.send_message
+  #
+  #     # link = response[:data]
+  #     # templateCode = '020050000281'
+  #     # content = "[MoveMore]\n"+link+" 님의 사용권이 소진되었습니다ㅠ\n\n아래 버튼으로 결제하시고 헬스장에\n있는 태블릿으로 체크인 하시면 됩니다:)\n\n\n무브모어 카카오톡 채널:\n@무브모어 @movemore"
+  #     # receiver = current_user.phone
+  #     # receiverName = current_user.phone.last(4)
+  #     # kakao_boot = KakaoAlarmService.new(templateCode, content, receiver, receiverName)
+  #     # kakao_boot.send_alarm
+  #   end
+  # end
 
   def pay_complete
 
