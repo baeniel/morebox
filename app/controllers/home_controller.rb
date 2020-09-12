3cfae049399656481c01e9f59048abc10c35a0b5
baeniel <jinveloper126@gmail.com>
2020-09-12 23:55:19 +0900

    칼로리 계산기

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,7 +20,8 @@ class HomeController < ApplicationController
         end
       else
         # redirect_to list_items_path
-        redirect_to root_path, notice: "접근 권한이 없습니다."
+        # redirect_to root_path, notice: "접근 권한이 없습니다."
+        redirect_to market_users_path
       end
     end
   end
