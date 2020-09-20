dfe212d6bd2bc0e61b0cae9b34e9618da2e92155
woongjae lee <winzett0@gmail.com>
2020-09-20 22:58:17 +0900

    시크릿 코드 분기 주석처리

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,12 @@ class HomeController < ApplicationController
   def index
     browser = Browser.new(request.env["HTTP_USER_AGENT"])
     #태블릿일 때 (추후 시크릿 코드 분기 필요)
-    if browser.device.tablet?
+    if browser.device.tablet? # && params[:secret_code] == "gorillabom"
       redirect_to list_items_path
     #핸드폰이나 데스크탑 접속할 때
     else
       if current_user.fit_center || current_user.manager?
-        if current_user.fit_center
-          redirect_to gym_path(current_user.gym)
-        elsif current_user.manager?
+        if current_user.manager?
           redirect_to users_path
         else
           redirect_to gym_path(current_user.gym)
