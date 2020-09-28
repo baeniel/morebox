7dec6329c6d8a36ac6c9d35762a4400aba963b4b
baeniel <jinveloper126@gmail.com>
2020-09-28 16:43:21 +0900

    트레이너 코드 번호 뒷 4자리

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -47,9 +47,16 @@ class ApisController < ApplicationController
                   price: @item.price
                 }]
       end
-      trainer = User.where(phone: params[:trainer_code], user_type: :manager).where.not(id: current_user&.id).first
-      if (params[:trainer_code].blank? || trainer)
+      # trainer = User.where(phone: params[:trainer_code], user_type: :manager).where.not(id: current_user&.id).first
+      @trainer = nil
+      trainers = User.manager.where(gym: current_gym).where.not(id: current_user&.id)
+      trainers.each do |trainer|
+        if (trainer.phone.last(4) == params[:trainer_code])
+          @trainer = User.find(trainer.id)
+        end
+      end
 
+      if (params[:trainer_code].blank? || @trainer)
         response = bootpay.request_payment(
           pg: 'inicis', # PG Alias
           method: 'card', # Method Alias
@@ -65,7 +72,7 @@ class ApisController < ApplicationController
           # return_url: "http://172.30.1.34:3003/users/#{current_user.id}/pay_complete"
           return_url: "https://morebox.co.kr/users/#{current_user.id}/pay_complete"
         )
-        if (order = current_user.orders.create(status: :ready, gym: current_gym, item: @item, order_number: order_number, trainer: trainer))
+        if (order = current_user.orders.create(status: :ready, gym: current_gym, item: @item, order_number: order_number, trainer: @trainer))
           @subitems.each do |sub_item|
             order.order_sub_items.new(quantity: subitem_info&.dig(sub_item.id.to_s)&.to_i, sub_item: sub_item)
           end
