f9d7134250f7bd084b65a21824b2cb5430fa8275
woongjae lee <winzett0@gmail.com>
2020-10-02 03:59:17 +0900

    fix logic

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -4,7 +4,6 @@ class DietsController < ApplicationController
 
     difference = params[:current_calorie].to_f - params[:target_calorie].to_f
     diets = Diet.all
-    
     @diet = if difference.abs <= 100
               diets.where("start_calorie > :calorie and end_calorie < :calorie", calorie: difference).first
             elsif difference > 600
@@ -12,7 +11,7 @@ class DietsController < ApplicationController
             elsif difference < -600
               diets.where(end_calorie: -600).where(protein: params[:protein]).where(purpose: params[:purpose]).first
             else
-              diets = diets.where("start_calorie > :calorie and end_calorie < :calorie", calorie: difference).where(protein: params[:protein]).where(purpose: params[:purpose])
+              diets = diets.where("start_calorie <= :calorie and end_calorie > :calorie", calorie: difference).where(protein: params[:protein]).where(purpose: params[:purpose])
               if difference < 0
                 diets = diets.where(snack: params[:snack].to_i > 0)
               end
