854e22a3431f2561988c86f2714f07e3d895dbf9
baeniel <jinveloper126@gmail.com>
2020-10-05 17:41:34 +0900

    change

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -17,13 +17,14 @@ class Gym < ApplicationRecord
   #이번 달 모어박스 매출
   def month_box_sale
     this_month = Date.today.month
-    self.orders.complete.includes(:item).map{|order| order.created_at.month.eql?(this_month) ? order&.item&.price.to_i : 0 }&.sum
+    self.orders.complete.map{|order| order.created_at.month.eql?(this_month) ? order&.payment_amount.to_i : 0 }&.sum
   end
 
   #이번 달 그 센터의 총 매출 => 추후 모어마켓에서 트레이너 코드 쓴 매출 포함시켜야 함.
   def month_center_sale
     this_month = Date.today.month
     month_market_sale = 0
-    self.orders.complete.includes(:item).map{|order| order.created_at.month.eql?(this_month) ? order&.item&.price.to_i : 0 }&.sum + month_market_sale
+    self.orders.complete.map{|order| order.created_at.month.eql?(this_month) ? order&.payment_amount.to_i : 0 }&.sum + month_market_sale
+    # self.orders.complete.includes(:item).map{|order| order.created_at.month.eql?(this_month) ? order&.item&.price.to_i : 0 }&.sum + month_market_sale
   end
 end
