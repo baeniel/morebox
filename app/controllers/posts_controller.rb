bd5c56edd139a92a3f34d3a7626e48f4d53399fc
baeniel <jinveloper126@gmail.com>
2020-10-27 18:41:13 +0900

    각 센터 대표님들께 알람

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     receiver = post.phone
     receiverName = post.name
     subject = "당신만을 위한 영양 상담"
-    contents = "[모어코치]\n"+"#{link}"+"\n 위 링크에 접속하셔서 '닉네임, 상담시작'이라고 카톡을 보내주세요:)\n\n ex)미키마우스, 상담시작"
+    contents = "[핏테이블]\n"+"#{link}"+"\n 위 링크에 접속하셔서 '닉네임, 상담시작'이라고 카톡을 보내주세요:)\n\n ex)미키마우스, 상담시작"
     nutrition_alarm = MessageAlarmService.new(receiver, receiverName, subject, contents)
     nutrition_alarm.send_message
 
