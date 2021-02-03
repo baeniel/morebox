4f1270f7122490793593982230833dea4e323f85
baeniel <jinveloper126@gmail.com>
2021-02-03 14:34:29 +0900

    change

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,33 +1,37 @@
 class CardsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_object, only: [:show, :update]
+  before_action :load_object, only: [:show, :update, :edit]
 
   def index
     @cards = Card.ready
     @cards = Card.complete if params[:status] == "complete"
   end
 
-  def new ;end
+  def new
+    @card = Card.new
+  end
 
   def create
     card = Card.new(card_params)
-    if card.save
+    card.importance = "할지안할지논의가필요해요"
+    if card.save!
       redirect_to cards_path, notice: "건의가 등록되었습니다."
-    else
-      redirect_to cards_path, alert: "다시 시도해주세요!"
     end
   end
 
   def show ;end
 
+  def edit; end
+
   def update
-    @card.complete!
-    redirect_to cards_path, notice: "태스크를 완료했습니다!"
+    if @card.update!(card_params)
+      redirect_to cards_path, notice: "태스크를 수정했습니다!"
+    end
   end
 
   private
   def card_params
-    params.require(:card).permit(:description, :importance, :category)
+    params.require(:card).permit(:description, :importance, :category, :status)
   end
 
   def load_object
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,9 @@
 class Card < ApplicationRecord
   validates :description, presence: true
-  validates :importance, presence: true
+  # validates :importance, presence: true
   validates :category, presence: true
 
-  enum importance: %i(아이디어제안 빠른시일내에반영됐으면좋겠어요 급합니다ㅠㅠ)
+  enum importance: %i(빠르게가능합니다(하루) 시간이좀걸려요(일주일) 시간이많이걸립니다ㅠ(한달) 할지안할지논의가필요해요)
   enum category: %i(개발 디자인 코칭 마케팅 기타)
   enum status: %i(ready complete)
 end
