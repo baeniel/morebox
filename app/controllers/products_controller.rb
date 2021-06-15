49d82def0290123395e3656e1c98b051e9baf81d
baeniel <jinveloper126@gmail.com>
2021-06-15 17:14:45 +0900

    change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,16 @@
 require "browser"
 
 class ProductsController < ApplicationController
+  before_filter :redirect_to_http
+
   def marketing
     sign_out
     @browser = Browser.new(request.env["HTTP_USER_AGENT"])
+  end
+
+  private
+
+  def redirect_to_http
+    redirect_to :protocol => "http://"
   end
 end
