a73a2da36e07bed7420c1ae60907e69a9daa3292
baeniel <jinveloper126@gmail.com>
2021-06-15 19:05:26 +0900

    change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,6 @@ class ProductsController < ApplicationController
   def marketing
     sign_out
     @browser = Browser.new(request.env["HTTP_USER_AGENT"])
-    # redirect_to "http://morebox.co.kr/"
+    redirect_to "http://morebox.co.kr/"
   end
 end
